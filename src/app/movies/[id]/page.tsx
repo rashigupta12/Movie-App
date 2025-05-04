fb6c12b05a8827eb96d7b676f94e27c4059510b1
rashigupta12 <108977620+rashigupta12@users.noreply.github.com>
2025-05-04 22:05:12 +0530

    params change in use movies[id]

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -1,16 +1,15 @@
 // app/(movies)/[id]/page.tsx
+'use client';
 import { MovieDetails } from '@/components/MovieDetails';
+import { useParams } from 'next/navigation';
 import { Suspense } from 'react';
 
-export default function MovieDetailPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default function MovieDetailPage() {
+  const { id } = useParams<{ id: string }>();
   return (
     <div className="container py-8">
       <Suspense fallback={<div>Loading movie details...</div>}>
-        <MovieDetails id={parseInt(params.id)} />
+        <MovieDetails id={parseInt(id)} />
       </Suspense>
     </div>
   );
